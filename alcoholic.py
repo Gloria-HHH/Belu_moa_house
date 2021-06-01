564d5107a04256faf40e1851cb777563e9ba03b8
Gloria-HHH <85085724+Gloria-HHH@users.noreply.github.com>
2021-06-01 00:11:04 -0400

    print the name

---
+++ alcoholic.py
@@ -0,0 +1,7 @@
+# Belu is a super 13 dian creature
+# He has a super addiction to alcohol products, especially Beers!!!
+# Even if it may not be a good time to consume it, Belu super insists on it and is too lazy to confirm the feasibility of drinking beers by simply making a phone call.
+
+print("mami")
+
+
